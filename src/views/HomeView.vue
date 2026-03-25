4f71f198e537e484a9dff431361a6586a593937e
Diego Reyes <diegorele13@gmail.com>
2026-03-25 13:54:38 -0500

    initial  commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import HeroSection from '@/components/HeroSection.vue';
+import FooterSection from '@/components/FooterSection.vue';
+</script>
+
+<template>
+  <div class="home-view">
+    <main>
+      <HeroSection />
+    </main>
+    <FooterSection />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.home-view {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  
+  main {
+    flex: 1;
+  }
+}
+</style>
